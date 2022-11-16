803c7de60ed9297fc58c493898fefb05f5c28f9f
Christian Reasoner <czr@hawaii.edu>
2022-11-15 15:40:17 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,18 +4,53 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
-
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
-
-    </Row>
+    <div className="landing-lightgreen-background">
+      <Container className="text-center">
+        <h2 style={{ color: 'white', fontSize: '30pt', fontWeight: '200' }}>
+          Welcome to Manoa Market
+        </h2>
+        <h3 style={{ paddingBottom: '20px', color: 'white', fontWeight: '200' }}>
+          Buy, Sell, and Trade items in the UH Community
+        </h3>
+      </Container>
+    </div>
   </Container>
 );
 
 export default Landing;
+
+/*
+import React from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import { PageIDs } from '../utilities/ids';
+
+const Landing = () => (
+  <div id={PageIDs.landingPage}>
+    <div className="landing-lightgreen-background">
+      <Container className="text-center">
+        <h2 style={{ color: 'white', fontSize: '30pt', fontWeight: '200' }}>
+          Welcome to Manoa Market
+        </h2>
+        <h3 style={{ paddingBottom: '20px', color: 'white', fontWeight: '200' }}>
+          Buy, Sell, and Trade items in the UH Community
+        </h3>
+      </Container>
+    </div>
+    <div className="landing-white-background">
+      <Container className="justify-content-center text-center">
+        <Row md={1} lg={2}>
+          <Col xs={3}>
+            <Image src="/images/furniture2.png" width={600} />
+          </Col>
+          <Col xs={3}>
+            <Image src="/images/furniture.png" width={600} height={420} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </div>
+);
+
+export default Landing;
+*/
+
