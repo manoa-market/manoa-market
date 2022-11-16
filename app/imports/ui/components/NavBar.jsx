d656dc696ac7c6e19e2069ac6ff04b95025ca883
Christian Reasoner <czr@hawaii.edu>
2022-11-15 15:36:21 -1000

    11/15

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
 import { Roles } from 'meteor/alanning:roles';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { BoxArrowRight, PersonFill, PersonPlusFill } from 'react-bootstrap-icons';
 
 const NavBar = () => {
@@ -15,8 +15,8 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={NavLink} to="/">
-          <h2>meteor-application-template-react</h2>
+        <Navbar.Brand as={NavLink} to="/" className="align-items-center">
+          <span style={{ fontWeight: 800, fontSize: '24px' }}><Image src="/images/logo.png" width={100} style={{ marginBottom: 0 }} /></span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -63,3 +63,53 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+/*
+<Navbar expand="lg" style={menuStyle} className={navbarClassName}>
+  <Container>
+    <Navbar.Brand as={NavLink} to="/" className="align-items-center">
+      <span style={{ fontWeight: 800, fontSize: '24px' }}><Image src="/images/logo.png" width={100} style={{ marginBottom: 0 }} /></span>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls={ComponentIDs.basicNavbarNav} />
+    <Navbar.Collapse id={ComponentIDs.basicNavbarNav}>
+      <Nav className="me-auto justify-content-start">
+        {currentUser ? (
+          <Nav.Link as={NavLink} id={ComponentIDs.homeMenuItem} to="/myprofile" key="myprofile">My Profile</Nav.Link>
+        ) : ''}
+        <Nav.Link as={NavLink} id={ComponentIDs.profilesMenuItem} to="/listings" key="listings">Listings</Nav.Link>
+        <Nav.Link as={NavLink} id={ComponentIDs.projectsMenuItem} to="/projects" key="projects">Projects</Nav.Link>
+        <Nav.Link as={NavLink} id={ComponentIDs.interestsMenuItem} to="/interests" key="interests">Interests</Nav.Link>
+        {currentUser ? (
+          [<Nav.Link as={NavLink} id={ComponentIDs.addProjectMenuItem} to="/addProject" key="addP">Add Project</Nav.Link>,
+            <Nav.Link as={NavLink} id={ComponentIDs.filterMenuItem} to="/filter" key="filter">Filter</Nav.Link>]
+        ) : ''}
+      </Nav>
+      <Nav className="justify-content-end">
+        {currentUser === '' ? (
+          <NavDropdown id={ComponentIDs.loginDropdown} title="Login">
+            <NavDropdown.Item id={ComponentIDs.loginDropdownSignIn} as={NavLink} to="/signin">
+              <PersonFill />
+              Sign
+              in
+            </NavDropdown.Item>
+            <NavDropdown.Item id={ComponentIDs.loginDropdownSignUp} as={NavLink} to="/signup">
+              <PersonPlusFill />
+              Sign
+              up
+            </NavDropdown.Item>
+          </NavDropdown>
+        ) : (
+          <NavDropdown id={ComponentIDs.currentUserDropdown} title={currentUser}>
+            <NavDropdown.Item id={ComponentIDs.currentUserDropdownSignOut} as={NavLink} to="/signout">
+              <BoxArrowRight />
+              {' '}
+              Sign
+              out
+            </NavDropdown.Item>
+          </NavDropdown>
+        )}
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+*/
