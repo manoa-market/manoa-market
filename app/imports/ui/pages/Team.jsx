52e0471c012f86c5f0a099c9f7035c5d5f2e8944
Melvin Alhambra <99471168+melvinjae@users.noreply.github.com>
2022-11-15 14:38:59 -1000

    added descriptions for the team

--- app/imports/ui/pages/Team.jsx
+++ app/imports/ui/pages/Team.jsx
@@ -11,16 +11,16 @@ const Team = () => (
             <Image src="https://avatars.githubusercontent.com/u/89875776?v=4" width="100px" />
             <Card.Title>Jamie Laurin</Card.Title>
           </Card.Header>
-          <Card.Body>This is a brief description</Card.Body>
+          <Card.Body>I am studying for a B.S. in Computer Science in the Department of Information and Computer Sciences at the University of Hawaii. I expect to graduate in Fall 2023.</Card.Body>
         </Card>
       </Col>
       <Col xs={4}>
         <Card className="h-100">
-          <Card.Header>
+          <Card.Header className="background-color: yellow">
             <Image src="https://avatars.githubusercontent.com/u/89759095?v=4" width="100px" />
             <Card.Title>Christian Reasoner</Card.Title>
           </Card.Header>
-          <Card.Body>This is a brief description</Card.Body>
+          <Card.Body>I am studying for a B.S. in Computer Science in the Department of Information and Computer Sciences at the University of Hawaii. I expect to graduate in Spring, 2024.</Card.Body>
         </Card>
       </Col>
       <Col xs={4}>
@@ -29,7 +29,7 @@ const Team = () => (
             <Image src="https://avatars.githubusercontent.com/u/10895962?v=4" width="100px" />
             <Card.Title>Daniel Luong</Card.Title>
           </Card.Header>
-          <Card.Body>This is a brief description</Card.Body>
+          <Card.Body>I am studying for a B.S. in Computer Engineering in the Department of Electrical Engineering at the University of Hawaii at Manoa. I expect to graduate in Fall, 2022.</Card.Body>
         </Card>
       </Col>
     </Row>
@@ -40,7 +40,7 @@ const Team = () => (
             <Image src="https://avatars.githubusercontent.com/u/112609431?v=4" width="100px" />
             <Card.Title>Evan Araki</Card.Title>
           </Card.Header>
-          <Card.Body>This is a brief description</Card.Body>
+          <Card.Body>I am studying for a B.A. in Computer Science in the Department of Information and Computer Sciences at the University of Hawaii. I expect to graduate in Spring, 2024.</Card.Body>
         </Card>
       </Col>
       <Col xs={4}>
@@ -49,7 +49,7 @@ const Team = () => (
             <Image src="https://avatars.githubusercontent.com/u/99471168?v=4" width="100px" />
             <Card.Title>Melvin Alhambra</Card.Title>
           </Card.Header>
-          <Card.Body>This is a brief description</Card.Body>
+          <Card.Body>I am a Computer Engineering senior at the University of Hawaii at Manoa. I expect to graduate Fall 2022.</Card.Body>
         </Card>
       </Col>
     </Row>
