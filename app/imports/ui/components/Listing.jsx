a1cff00e5d007fd512e54a11d652b7f21c983cb5
Melvin Alhambra <99471168+melvinjae@users.noreply.github.com>
2022-11-22 11:44:45 -1000

    Update Listing.jsx

--- app/imports/ui/components/Listing.jsx
+++ app/imports/ui/components/Listing.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
 import PropTypes from 'prop-types';
 import { Card, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 /** Renders a single row in the List Listings table. See pages/ListListing.jsx. */
 const Listing = ({ listing }) => (
@@ -10,8 +8,8 @@ const Listing = ({ listing }) => (
     <Card.Header>
       <Card.Title>{listing.itemName}</Card.Title>
       <Card.Subtitle>Price: ${listing.price}</Card.Subtitle>
-      <Card.Subtitle>Condition {listing.condition}</Card.Subtitle>
-      <Card.Subtitle>Owner {listing.owner}</Card.Subtitle>
+      <Card.Subtitle>Condition: {listing.condition}</Card.Subtitle>
+      <Card.Subtitle>Owner: {listing.owner}</Card.Subtitle>
     </Card.Header>
     <Card.Body>
       <Image src={listing.itemImage} width={100} />
