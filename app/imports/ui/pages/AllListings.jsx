79d0c43204a4814c706d81a21bc4fddd90a5ed8c
Daniel Luong <dnlluong@gmail.com>
2022-11-28 00:12:13 -1000

    all-listing-navbar

--- app/imports/ui/pages/AllListings.jsx
+++ app/imports/ui/pages/AllListings.jsx
@@ -13,7 +13,7 @@ const AllListings = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Listings.userPublicationName);
+    const subscription = Meteor.subscribe(Listings.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
