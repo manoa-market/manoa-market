7224d9cb86613e87fde46119d7d5c82bb06bc166
Christian Reasoner <czr@hawaii.edu>
2022-11-29 17:46:55 -1000

    11/29

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Image, Row, Button } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -32,37 +32,3 @@ const Landing = () => (
 
 export default Landing;
 
-/*
-import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
-import { PageIDs } from '../utilities/ids';
-
-const Landing = () => (
-  <div id={PageIDs.landingPage}>
-    <div className="landing-lightgreen-background">
-      <Container className="text-center">
-        <h2 style={{ color: 'white', fontSize: '30pt', fontWeight: '200' }}>
-          Welcome to Manoa Market
-        </h2>
-        <h3 style={{ paddingBottom: '20px', color: 'white', fontWeight: '200' }}>
-          Buy, Sell, and Trade items in the UH Community
-        </h3>
-      </Container>
-    </div>
-    <div className="landing-white-background">
-      <Container className="justify-content-center text-center">
-        <Row md={1} lg={2}>
-          <Col xs={3}>
-            <Image src="/images/furniture2.png" width={600} />
-          </Col>
-          <Col xs={3}>
-            <Image src="/images/furniture.png" width={600} height={420} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  </div>
-);
-
-export default Landing;
-*/
